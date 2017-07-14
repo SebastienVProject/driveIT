c526aa28114098e401f7cf70b4cb9979aa881532
SebastienVProject <vergnollesebastien@icloud.com>
2017-07-14 10:13:14 +0200

    utilisation d'une tableViewController pour les taches --> OK

---
+++ driveIt/TachesTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TachesTableViewCell.swift
+//  driveIt
+//
+//  Created by utilisateur on 14/07/2017.
+//  Copyright © 2017 SVInfo. All rights reserved.
+//
+
+import UIKit
+
+class TachesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nomLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- driveIt/TachesTableViewController.swift
+++ driveIt/TachesTableViewController.swift
@@ -1,27 +1,24 @@
 //
-//  TachesViewController.swift
+//  TachesTableViewController.swift
 //  driveIt
 //
-//  Created by utilisateur on 24/06/2017.
+//  Created by utilisateur on 14/07/2017.
 //  Copyright © 2017 SVInfo. All rights reserved.
 //
 
 import UIKit
-import Firebase
 import FirebaseDatabase
 
-class TachesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class TachesTableViewController: UITableViewController {
 
     var taches = [Task]()
     static var currentExigence: String!
     
-    @IBOutlet weak var tableViewTache: UITableView!
-    
     override var prefersStatusBarHidden: Bool { return true }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let referenceTable = DataService.dataService.TASKS.queryOrdered(byChild: "idRequirement").queryEqual(toValue: TachesViewController.currentExigence)
         
         referenceTable.observe(DataEventType.value, with: { (snapshot) in
@@ -41,7 +38,7 @@ class TachesViewController: UIViewController, UITableViewDelegate, UITableViewDa
                     self.taches.append(currentTache)
                 }
                 
-                self.tableViewTache.reloadData()
+                self.tableView.reloadData()
             }
         })
     }
@@ -51,19 +48,25 @@ class TachesViewController: UIViewController, UITableViewDelegate, UITableViewDa
         // Dispose of any resources that can be recreated.
     }
 
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return taches.count
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let Cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let Cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! TachesTableViewCell
         
         let currentTache: Task = taches[indexPath.row]
         
-        Cell.textLabel?.text = currentTache.name
+        Cell.nomLabel.text = currentTache.name
         return Cell
     }
-    
+
     @IBAction func AddTapped(_ sender: UIBarButtonItem) {
         
         let alert = UIAlertController(title: "Ajouter une tache", message: "Merci de saisir les informations concernant la tache à créer", preferredStyle: .alert)
@@ -91,4 +94,50 @@ class TachesViewController: UIViewController, UITableViewDelegate, UITableViewDa
             referenceTable.child(code).setValue(AAjouter)
         }
     }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
